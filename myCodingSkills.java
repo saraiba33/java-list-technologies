f11b543253f81e5a22b096a62bca8760a60e5527
Sara Ibarra <saraiba33@gmail.com>
2022-05-25 09:45:32 -0600

    Add solution

---
+++ myCodingSkills.java
@@ -0,0 +1,11 @@
+class MyCodingSkills {
+
+    public static void main(String[] args) {
+        String[] skills = { "html", "CSS", "JavaScript", "TypeScript", "Node.js", "Angular", "functional programming",
+                "procedural programming" };
+
+        for (String skill : skills) {
+            System.out.println("I have learned " + skill);
+        }
+    }
+}
